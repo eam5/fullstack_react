6e51d798d7aba6779dec030eb7c5279929d8a433
eam5 <eam5hc@virginia.edu>
2019-11-19 15:54:24 -0500

    d. complex state

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,55 +1,56 @@
 import React, { useState} from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = ({ name,age }) => {
-    const bornYear = () => new Date().getFullYear() - age
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                press a button
+            </div>
+        )
+    }
 
     return (
-    <div>
-    <p>Hello {name}, you are {age} years old</p>
-    <p>So you were probably born in {bornYear()}</p>
-    </div>
+        <div>
+            button history: {props.allClicks.join(' ')}
+        </div>
     )
 }
 
-const Display = ({ counter }) => {
-    return (
-        <div>{counter}</div>
-    )
-}
+const Button = (props) => {
+    console.log(props)
+    const { onClick, text } = props
 
-const Button = ({ onClick, text }) => {
     return (
-        <button onClick={onClick}>
-            {text}
-        </button>
+        <button onClick={onClick}>{text}</button>
+
     )
-}
+    }
 
 const App = (props) => {
-    const name = 'Mark'
-    const age = 40
-    const [ counter, setCounter ] = useState(0)
-    const setToValue = (value) => setCounter(value)
-    //Using the above setToValue 'double function' not necessarily better than below functions, just example of currying techinque
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
-    // const increaseByOne = () =>
-    //     setCounter(counter + 1)
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
 
-    // const setToZero = () =>
-    //     setCounter(0)
-    
     return ( 
-    <>
-        <h1>Greetings</h1>
-        <Hello name="Gerard" age={21+14}/>
-        <Hello name={name} age={age}/>
-
-        <Display counter={counter}/>
-        <Button onClick={() => setToValue(counter + 1)} text='plus' />
-        <Button onClick={() => setToValue(0)} text='zero' />
-        <Button onClick={() => setToValue(counter - 1)} text='minus' />
-    </>
+        <div>
+            <div>
+                {left}
+                <Button onClick={handleLeftClick} text='left' />
+                <Button onClick={handleRightClick} text='right' />
+                {right}
+                <History allClicks={allClicks} />
+            </div>
+        </div>
 ) }
 
 
