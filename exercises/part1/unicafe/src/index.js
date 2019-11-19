18ad9c75a5ab69b26fe1701a57ef88fd0314e622
eam5 <eam5hc@virginia.edu>
2019-11-19 18:30:44 -0500

    1.8: unicafe step3

--- exercises/part1/unicafe/src/index.js
+++ exercises/part1/unicafe/src/index.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistics = (props) => {
+    return (
+    <div>
+        <h1>stats</h1>
+        <p>good: {props.good}</p>
+        <p>neutral: {props.neutral}</p>
+        <p>bad: {props.bad}</p>
+        <p>all: {props.all}</p>
+        <p>average: {props.result}</p>
+        <p>positive: {props.percentGood}%</p>
+    </div>
+    )
+  }
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -28,14 +42,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-
-      <h2>stats</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>average: {result}</p>
-      <p>positive: {percentGood}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} result={result} percentGood={percentGood} />
     </div>
   )
 }
