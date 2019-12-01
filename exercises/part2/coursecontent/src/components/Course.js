0eff2242a602f83f25779f34badaa47c5eed182c
eam5 <eam5hc@virginia.edu>
2019-12-01 16:43:55 -0500

    2.3*: Course contents step8

--- exercises/part2/coursecontent/src/components/Course.js
+++ exercises/part2/coursecontent/src/components/Course.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 
 const Course = ({course}) => {
@@ -9,6 +10,7 @@ const Course = ({course}) => {
         <div>
             <Header course={course.name}/>
             <Content content={course.parts}/>
+            <Total total={course.parts} />
         </div>
     )
 }
---
+++ exercises/part2/coursecontent/src/components/Total.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Total = ({ total }) => {
+    const totalAmount = total.reduce(function(sum, part) {
+        console.log("sum", sum, part)
+        return sum + part.exercises
+    }, 0)
+    return (
+        <p>Total number of exercises {totalAmount}</p>
+    )
+}
+
+export default Total
