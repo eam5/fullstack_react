68c5894c9de617f1f92fa12c543783ad8c5dc480
eam5 <eam5hc@virginia.edu>
2019-12-04 14:30:40 -0500

    2.11: The Phonebook Step6- fixed

--- exercises/part2/phonebook/src/components/Contacts.js
+++ exercises/part2/phonebook/src/components/Contacts.js
@@ -1,17 +1,14 @@
 import React from 'react'
 
 const Contacts = ({list, searchTerm}) => {
-    const rows = () => list.filter(filterBy(searchTerm)).map(list =>
-    <div key={list.name}>{list.name}, {list.number}</div>
-    )
+    const rows = () => list
+      .filter(list => 
+        list.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      .map(list =>
+        <div key={list.name}>{list.name}, {list.number}</div>
+      )
 
-    const filterBy = (term) => {
-        const termLowerCase = term.toLowerCase()
-        return (person) =>
-          Object.keys(person)
-            .some(prop => person[prop].toLowerCase().indexOf(termLowerCase) !== -1)
-      }
-    
     return (
         <div>{rows()}</div>
     )
