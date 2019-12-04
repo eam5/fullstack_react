88842a685ff85e68750f91aec010d7ed9d8b36a4
eam5 <eam5hc@virginia.edu>
2019-12-04 13:39:53 -0500

    2.11: The Phonebook Step6

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,10 +1,34 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
+  // useEffect(() => {
+  //   console.log('effect')
+  //   axios
+  //     .get('http://localhost:3001/notes')
+  //     .then(response => {
+  //       console.log('promise fulfilled')
+  //       setNotes(response.data)
+  //     })
+  // }, [])
+  // console.log('render', notes.length, 'notes')
 
   const notesToShow = showAll
     ? notes
@@ -31,7 +55,7 @@ const App = (props) => {
   }
 
   const handleNoteChange = (event) => {
-    // console.log(event.target.value)
+    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
