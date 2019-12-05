4c22181eac5a9d3745ea4d9cc09fe8ce8ca5a496
eam5 <eam5hc@virginia.edu>
2019-12-05 14:35:54 -0500

    2.14*: Data for countries, step3 - language list fixed, weather only for Finland

--- exercises/part2/countrydata/src/App.js
+++ exercises/part2/countrydata/src/App.js
@@ -9,7 +9,29 @@ const Filter = (props) => {
   )
 }
 
-const Countries = ({list, searchTerm, setSearchTerm, showCountry}) => {
+const Weather = ({place, weather}) => {
+  return (
+  <div>
+  <h3>Current weather in {place.capital} </h3>
+  <p>Temperature: {weather.temperature} Celsius</p>
+  <img src={weather.weather_icons} alt='weather icon' />
+  <p>Wind: {weather.wind_speed} kph {weather.wind_dir}</p>
+  </div>
+  )
+}
+
+const Languages= ({ content }) => {
+  const langlist = () => content.map(part =>
+    <li key={part.name}>{part.name}</li>
+    )
+  return (
+      <div>
+          {langlist()}
+      </div>
+  )
+}
+
+const Countries = ({list, weather, searchTerm, setSearchTerm, showCountry}) => {
   const countriesToShow = showCountry
     ? list
     : list.filter(list => list.name.toLowerCase().includes(searchTerm.toLowerCase()) )
@@ -32,9 +54,10 @@ const Countries = ({list, searchTerm, setSearchTerm, showCountry}) => {
         <div>Population: {list.population}</div>
         <h3>Languages</h3>
         <ul>
-          <li>{list.languages.name}</li>
+        <Languages content={list.languages} />
         </ul>
         <img src={list.flag} width='150px' alt='country flag'/>
+        <Weather place={list} weather={weather}/>
       </div>
       )
 
@@ -50,15 +73,24 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   const [showCountry, setShowCountry] = useState(searchTerm)
-// console.log(countries[0])
-// console.log(showCountry)
+  const [weather, setWeather] = useState([])
 console.log(searchTerm)
+console.log(weather)
 
 const hook = () => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
         setCountries(response.data)
+      })
+    const params = {
+      access_key: process.env.REACT_APP_WEATHER_API_KEY,
+      query: 'Helsinki'
+    }
+    axios
+      .get('http://api.weatherstack.com/current', {params})
+      .then(response => {
+        setWeather(response.data)
       })
   }
 
@@ -76,9 +108,11 @@ const hook = () => {
         />
         <Countries 
           list={countries} 
+          languages={countries.languages}
           searchTerm={searchTerm} 
           setSearchTerm={setSearchTerm}
           showCountry={showCountry}
+          weather={weather.current}
         />
     </div>
   )
