6d5532482f049418931f75034016dfbabfdea834
eam5 <eam5hc@virginia.edu>
2019-12-12 13:28:05 -0500

    update part3

--- part3/index.js
+++ part3/index.js
@@ -1,90 +1,101 @@
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
 let notes = [
-    {
-    "id": 1,
-    "content": "HTML is easy",
-    "date": "2019-05-30T17:30:31.098Z",
-    "important": true
-    },
-    {
-    "id": 2,
-    "content": "Browser can execute only Javascript",
-    "date": "2019-05-30T18:39:34.091Z",
-    "important": true
-    },
-    {
-    "id": 3,
-    "content": "GET and POST are the most important methods of HTTP protocol",
-    "date": "2019-05-30T19:20:14.298Z",
-    "important": true
-    }
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
 const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
 }
 
 app.post('/notes', (request, response) => {
-    const body = request.body
-    
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
+  const body = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
-    notes = notes.concat(note)
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
 
-    response.json(note)
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
-app.get('/notes/', (request, response) => {
-    response.json(notes)
+app.get('/notes', (request, response) => {
+  response.json(notes)
 })
 
 app.get('/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => {
-        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-        return note.id === id
-    })
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-    console.log(note) 
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
-
 
 app.delete('/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end()
+  response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
