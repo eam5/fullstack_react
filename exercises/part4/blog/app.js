f2a8e67d55ea320ba5da2660e73193a2a96172ae
eam5 <eam5hc@virginia.edu>
2020-01-08 16:01:36 -0500

    part4-a, 4.1-4.2 Blog steps 1-2

---
+++ exercises/part4/blog/app.js
@@ -0,0 +1,16 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
